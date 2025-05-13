0831925575f4a1ce8084c075c6156db34847912d
IsaamUddin1996 <isaam1996.mnm@gmail.com>
2025-05-14 04:07:50 +0500

    Commit

--- Data Types/Test.py
+++ Data Types/Test.py
@@ -1,50 +1,10 @@
-# class Atm:
+import requests
 
-#     def __init__(self):
-#         self.balance = 0
-#         self.pin = ""
+url = "https://www.artificial-intelligence.blog/"
+response = requests.get(url)
 
-#         self.menu()
+# Print response headers to check content type
+print("Content-Type:", response.headers.get("Content-Type"))
 
-#     def menu(self):
-#         user_input = "Enter Your option"
-
-#         if user_input == 1:
-#             self.create_pin()
-#         elif user_input == 2:
-#             self.withdraw()
-#         if user_input == 3:
-#             self.deposit()
-#         elif user_input == 4:
-#             self.balance()
-#         else:
-#             print("Bye")
-
-#     def create_pin(self):
-#         self.pin = input("Enter your pin")
-#         print(f"Print Set Successfully {self.pin}")
-#     def withdraw(self):
-#         if self.balance:
-#             withdraw_amount = int(input("Enter the amount you want to withdraw: "))
-#             self.balance = self.balance - withdraw_amount
-#         print(f"Your remaining Balance is {self.balance}")
-
-#     def deposit(self):
-#         Amount = int(input("Enter the amount you want to deposit"))
-#         self.balance = self.balance + Amount
-#         print(f"Your Amount: {self.balance}")
-
-#     def balance(self):
-#         print(self.balance)
-
-class Atm:
-    counter = 0
-
-    def __init__(self):
-        self.sno = Atm.counter
-        Atm.counter +=1
-
-classatm = Atm()
-class2atm = Atm()
-print(classatm.sno)
-print(class2atm.sno)
+# Print the actual response text (for debugging)
+print("Response Text:", response.text[:200])  # Print only first 200 characters
