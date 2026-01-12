fd718224f986fe785e55ef2828ca5f6aaa1db6be
IsaamUddin1996 <isaam1996.mnm@gmail.com>
2026-01-12 19:45:59 +0500

    Interview update

--- Interview.py
+++ Interview.py
@@ -37,3 +37,31 @@ def is_even(n):
 
 print(is_even(4))   # True
 print(is_even(7))   # False
+
+
+# Reverse a String
+
+# Python – Using a Loop (manual method)
+
+def reverse_string(s):
+    rev = ""
+    for ch in s:
+        rev = ch + rev
+    return rev
+
+print(reverse_string("hello"))   # olleh
+
+# Python – Two-Pointer Method (best logically)
+
+def reverse_string(s):
+    s = list(s)
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+
+    return "".join(s)
+
+print(reverse_string("hello"))
